695baf99f9e152de9a91514711aef5454a5637e3
Adriano Foschi <adri.foschi@gmail.com>
2023-12-12 23:29:29 +0100

    change language codes

--- components/i18n/i18next.js
+++ components/i18n/i18next.js
@@ -1,14 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import it from "./it";
-import en from "./en";
+import it_IT from "./it_IT";
+import en_US from "./en_US";
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-    "en-GB":en,
-    "it-IT":it
+    en_US,
+    it_IT
 };
 
 const detectedLanguage = navigator.language || navigator.userLanguage;
@@ -17,10 +17,10 @@ i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: detectedLanguage, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+        lng: detectedLanguage.replace('-','_'), // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
         // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
         // if you're using a language detector, do not define the lng option
-        fallbackLng: "en-GB",
+        fallbackLng: "en_US",
         interpolation: {
             escapeValue: false // react already safes from xss
         }
