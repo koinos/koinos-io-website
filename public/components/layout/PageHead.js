14881a4a3aedb4ceff25da07ce67fa12b5c96cc9
Ron Hamenahem <ron@93nyc.com>
2024-04-19 14:10:05 -0400

    add google analytics

--- public/components/layout/PageHead.js
+++ public/components/layout/PageHead.js
@@ -1,15 +1,28 @@
 import Head from 'next/head'
 
 const PageHead = ({ headTitle }) => {
-    return (
+  return (
+    <>
+      <Head>
         <>
-            <Head>
-                <>
-                    {headTitle ? headTitle : "Martex - Software, App, SaaS & Startup Nextjs Landing Pages Pack"}
-                </>
-            </Head>
+          <title>{headTitle ? headTitle : "Martex - Software, App, SaaS & Startup Nextjs Landing Pages Pack"}</title>
+          
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5Z72Z9YG2G"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-348XXW1GR2');
+              `,
+            }}
+          />
         </>
-    )
+      </Head>
+    </>
+  )
 }
 
 export default PageHead
