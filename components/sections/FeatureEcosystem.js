499289b51f4a3101b3c22ba89bfc228b3daec700
Steve Gerbino <steve@gerbino.co>
2024-06-14 20:12:48 +0200

    #69: Use dark background images for Koiner and KoinCity

--- components/sections/FeatureEcosystem.js
+++ components/sections/FeatureEcosystem.js
@@ -60,7 +60,7 @@ export default function FeatureEcosystem() {
                                                 <div className="shape-ico color--theme">
                                                     {/* Vector Icon */}
                                                     {/*<span className="flaticon-pay-per-click" />*/}
-                                                    <img src="images/dapps/koiner.png"/>
+                                                    <img src="images/dapps/koiner-mark.png"/>
                                                     {/* Shape */}
                                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
@@ -80,7 +80,7 @@ export default function FeatureEcosystem() {
                                                 <div className="shape-ico color--theme">
                                                     {/* Vector Icon */}
                                                     {/*<span className="flaticon-taxes" />*/}
-                                                    <img src="images/dapps/koincity.png"/>
+                                                    <img src="images/dapps/koincity-mark.png"/>
                                                     {/* Shape */}
                                                     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z" transform="translate(100 100)" />
