5b502c7bffb42726a0ee1828f903a9650e688b6c
Ron Hamenahem <ron@93nyc.com>
2024-07-18 06:43:37 -0700

    #88 add bingx to the list of exchanges, clean up comments in exchange component, sort exchange list according to cmc ranking

--- components/sections/WhereToGetKOIN.js
+++ components/sections/WhereToGetKOIN.js
@@ -15,21 +15,16 @@ export default function WhereToGetKOIN() {
         <>
             <section id="integrations-1" className="pt-100 integrations-section" ref={sectionRef}>
                 <div className="container">
-                    {/* SECTION TITLE */}
                     <div className="row justify-content-center">
                         <div className="col-md-8">
                             <div className="section-title mb-70">
-                                {/* Title */}
                                 <h2 className="s-50 w-700">Where to get KOIN</h2>
-                                {/* Text */}
                                 <p className="s-21 color--grey">Find Koinos on Exchanges Across the Globe</p>
                             </div>
                         </div>
                     </div>
-                    {/* INTEGRATIONS-1 WRAPPER */}
                     <div className="integrations-1-wrapper">
                         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 rows-2">
-                            {/* TOOL #1 */}
                             <div className="col">
                                 <Link href="https://www.mexc.com/exchange/KOIN_USDT" className="in_tool it-1 r-12 mb-30 wow fadeInUp">
                                     {/* Icon */}
@@ -44,24 +39,22 @@ export default function WhereToGetKOIN() {
                                         <p className="p-sm">CEX</p>
                                     </div>
                                 </Link>
-                            </div>	{/* END FEATURE BOX #1 */}
-                            {/* TOOL #2 */}
+                            </div>
                             <div className="col">
-                                <Link href="https://www.coinstore.com/#/spot/KOINUSDT" className="in_tool it-2 r-12 mb-30 wow fadeInUp">
+                                <Link href="https://bingx.com/en/spot/KOINUSDT/" className="in_tool it-5 r-12 mb-30 wow fadeInUp">
                                     {/* Icon */}
                                     <div className="in_tool-logo-wrap">
                                         <div className="in_tool-logo ico-60">
-                                            <img className="img-fluid" src="/images/buy/coinstore-logo.png" alt="Coinstore Logo" />
+                                            <img className="img-fluid" src="/images/buy/bingx-logo.png" alt="BingX Logo" />
                                         </div>
                                     </div>
                                     {/* Text */}
                                     <div className="in_tool-txt">
-                                        <h6 className="s-20 w-700">Coinstore</h6>
+                                        <h6 className="s-20 w-700">BingX</h6>
                                         <p className="p-sm">CEX</p>
                                     </div>
                                 </Link>
-                            </div>	{/* END FEATURE BOX #2 */}
-                            {/* TOOL #3 */}
+                            </div>
                             <div className="col">
                                 <Link href="https://www.biconomy.com/exchange/KOIN_USDT" className="in_tool it-3 r-12 mb-30 wow fadeInUp">
                                     {/* Icon */}
@@ -76,8 +69,22 @@ export default function WhereToGetKOIN() {
                                         <p className="p-sm">CEX</p>
                                     </div>
                                 </Link>
-                            </div>	{/* END FEATURE BOX #3 */}
-                            {/* TOOL #4 */}
+                            </div>
+                            <div className="col">
+                                <Link href="https://www.coinstore.com/#/spot/KOINUSDT" className="in_tool it-2 r-12 mb-30 wow fadeInUp">
+                                    {/* Icon */}
+                                    <div className="in_tool-logo-wrap">
+                                        <div className="in_tool-logo ico-60">
+                                            <img className="img-fluid" src="/images/buy/coinstore-logo.png" alt="Coinstore Logo" />
+                                        </div>
+                                    </div>
+                                    {/* Text */}
+                                    <div className="in_tool-txt">
+                                        <h6 className="s-20 w-700">Coinstore</h6>
+                                        <p className="p-sm">CEX</p>
+                                    </div>
+                                </Link>
+                            </div>
                             <div className="col">
                                 <Link href="https://exchange.lcx.com/trade/KOIN-EUR" className="in_tool it-4 r-12 mb-30 wow fadeInUp">
                                     {/* Icon */}
@@ -92,8 +99,7 @@ export default function WhereToGetKOIN() {
                                         <p className="p-sm">CEX</p>
                                     </div>
                                 </Link>
-                            </div>	{/* END FEATURE BOX #4 */}
-                            {/* TOOL #5 */}
+                            </div>	
                             <div className="col">
                                 <Link href="https://app.koindx.com/swap" className="in_tool it-5 r-12 mb-30 wow fadeInUp">
                                     {/* Icon */}
@@ -108,10 +114,10 @@ export default function WhereToGetKOIN() {
                                         <p className="p-sm">DEX</p>
                                     </div>
                                 </Link>
-                            </div>	{/* END FEATURE BOX #5 */}
+                            </div>	
                         </div>
-                    </div>	{/* END INTEGRATIONS-1 WRAPPER */}
-                </div>     {/* End container */}
+                    </div>	
+                </div> 
             </section>
         </>
     )
