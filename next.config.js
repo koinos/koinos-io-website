5a12009c5eb2b122887dff0b473aeca8b9bb5079
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2024-08-06 14:11:10 -0600

    #95: Cleanup and add Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
           {
             key: 'X-Frame-Options',
             value: 'DENY'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';"
           }
         ]
       }
