33a7b649452f5b9965e6f72a87a253fc05485d71
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2024-08-06 14:05:40 -0600

    #95: Try setting X-Frame-Option via next.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
