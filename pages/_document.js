52e057b7440f08f957e445f2f328b3298ab3deba
Steve Gerbino <steve@gerbino.co>
2024-09-24 20:10:55 +0200

    Use png for favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
