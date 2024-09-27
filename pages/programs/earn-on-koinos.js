67476cb6006ba359983ade88041a8006807f4134
Steve Gerbino <steve@gerbino.co>
2024-09-27 17:49:10 +0200

    Add elements to Earn on Koinos landing page, fix text size on smaller screens for hero, correct colors that did not match them on homepage

--- pages/programs/earn-on-koinos.js
+++ pages/programs/earn-on-koinos.js
@@ -2,7 +2,17 @@ import Layout from "@/components/layout/Layout";
 import Link from "next/link";
 import { useProgramStore } from "@/store/programStore"; // Import the Zustand store
 
+import AOS from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
 export default function EarnOnKoinos() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   // Fetch the earnOnKoinos program data from the store
   const program = useProgramStore((state) => state.programs.earnOnKoinos);
 
@@ -23,17 +33,16 @@ export default function EarnOnKoinos() {
               <div className="row justify-content-center">
                 <div className="col-lg-11 col-xl-10">
                 <div className="project-description txt-block">
-                  {/* Title and Program Overview */}
-                  <h2 className="s-46 w-700">
-                    {program.title}
-                  </h2>
-                  {/* Preview Image */}
-                  <div className="project-priview-img mb-50">
+                  <div className="project-preview-img mb-50">
                     <section className="mb-100 ct-01 content-section features-section division">
                       <div className="container">
                         <div className="row d-flex align-items-center">
                           <div className="col-md-6 order-last order-md-2">
-                            <div className="txt-block left-column wow fadeInRight">
+                            <div className="txt-block left-column wow" data-aos='fade-left'>
+                              {/* Title and Program Overview */}
+                              <h2 className="s-46 w-700">
+                                {program.title}
+                              </h2>
                               {/* Render the short description here */}
                               <p>
                                 {program.shortDescription}{" "}
@@ -67,7 +76,8 @@ export default function EarnOnKoinos() {
                             </div>
                           </div>
                           <div className="col-md-6 order-first order-md-2">
-                            <div className="img-block right-column wow fadeInLeft">
+                            <div className="img-block right-column wow" data-aos='fade-right'>
+                              {/* Image */}
                               <img
                                 className="img-fluid rounded"
                                 src={program.images.banner}
@@ -81,7 +91,7 @@ export default function EarnOnKoinos() {
                   </div>
 
                   {/* Program Description */}
-                  <div className="project-txt">
+                  <div className="project-txt mt-5">
                     <p>{program.description}</p>
 
                     {/* List of features */}
@@ -137,11 +147,11 @@ export default function EarnOnKoinos() {
                   <div className="mt-5">
                     {/* Tutorial Section */}
                     <div className="mt-5">
-                    <h2 className="s-38 w-700">Earn on Koinos Tutorial</h2>
+                    <h2 className="s-38 w-700 my-4">Earn on Koinos Tutorial</h2>
                     <p>Welcome to this quick guide on how to participate in the Earn on Koinos promotion, where you can earn up to 16% yield on your USDT by bridging it from Ethereum to Koinos and providing liquidity on KoinDX. We will do this in three steps, let's dive right in!</p>
 
                     {/* Step 1 */}
-                    <h3 className="s-30 w-700 mt-4">Step 1: Acquiring KOIN and Setting Up Kondor</h3>
+                    <h3 className="s-30 w-700 my-4">Step 1: Acquiring KOIN and Setting Up Kondor</h3>
                     <p>Before we start, you'll need to have some KOIN in your wallet and set up the Kondor wallet to interact with the Koinos blockchain.</p>
                     <ul>
                         <li><strong>Acquire KOIN:</strong>
@@ -159,7 +169,7 @@ export default function EarnOnKoinos() {
                     </ul>
 
                     {/* Step 2 */}
-                    <h3 className="s-30 w-700 mt-4">Step 2: Bridging USDT, ETH, or BTC to Koinos</h3>
+                    <h3 className="s-30 w-700 my-4">Step 2: Bridging USDT, ETH, or BTC to Koinos</h3>
                     <ul>
                         <li>Access the <Link href="https://dapp.chainge.finance/?fromChain=ETH&toChain=KOIN&fromToken=USDT&toToken=USDT" className="underline">Chainge Bridge</Link>.</li>
                         <li>Connect your source chain wallet.</li>
@@ -168,17 +178,60 @@ export default function EarnOnKoinos() {
                     </ul>
 
                     {/* Step 3 */}
-                    <h3 className="s-30 w-700 mt-4">Step 3: Providing Liquidity on KoinDX</h3>
+                    <h3 className="s-30 w-700 my-4">Step 3: Providing Liquidity on KoinDX</h3>
                     <ul>
                         <li>Visit <Link href="https://app.koindx.com" className="underline">KoinDX</Link> and connect your Kondor wallet.</li>
                         <li>Select USDT, ETH, or BTC and KOIN pairing to provide liquidity.</li>
                         <li>Click on "Add Position" to confirm and sign the transaction.</li>
                     </ul>
 
-                    <h3 className="s-30 w-700 mt-4">Conclusion</h3>
-                    <p>And that's it! You've successfully participated in the Earn on Koinos promotion, setting yourself up to earn up to 16% yield on your USDT, ETH, or BTC. By following these simple steps, you've acquired KOIN, set up your Kondor wallet, bridged your USDT from Ethereum to Koinos, and provided liquidity on KoinDX. Keep an eye on your positions and enjoy watching your crypto grow!</p>
-                    </div>
+                    <h3 className="s-30 w-700 my-4">Conclusion</h3>
+                    <p className="mb-5">And that's it! You've successfully participated in the Earn on Koinos promotion, setting yourself up to earn up to 16% yield on your USDT, ETH, or BTC. By following these simple steps, you've acquired KOIN, set up your Kondor wallet, bridged your USDT from Ethereum to Koinos, and provided liquidity on KoinDX. Keep an eye on your positions and enjoy watching your crypto grow!</p>
 
+
+                    {/* Join our community */}
+                      <div className="project-preview-img my-5">
+                        <section className="mb-100 ct-01 content-section features-section division">
+                          <div className="container">
+                            <div className="row d-flex align-items-center">
+                              <div className="col-md-6 order-last order-md-2">
+                                <div className="txt-block left-column wow" data-aos='fade-left'>
+                                  <span className="section-id">Community</span>
+                                  <h2 className="s-39 w-700">Join the Community</h2>
+                                  <p>
+                                  If you have any questions, join our community and ask! You will find that the Koinos community is both friendly and helpful, don't be shy. We look forward to meeting you!
+                                  </p>
+                                  <div className="btns-group">
+                                    <Link
+                                    href="https://telegram.koinos.io/" target="_blank"
+                                    className="btn r-04 btn--theme btn--primary mt-20"
+                                    >
+                                      Join Telegram{" "}<i className="fa-brands fa-telegram fa-lg" />
+                                    </Link>
+                                    <Link
+                                      href="https://discord.koinos.io/"
+                                      target="_blank"
+                                      className="btn r-04 btn--theme btn--secondary"
+                                    >
+                                      Join Discord{" "}<i className="fa-brands fa-discord fa-lg" />
+                                    </Link>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="col-md-6 order-first order-md-2">
+                                <div className="img-block right-column wow" data-aos='fade-right'>
+                                  <img
+                                    className="img-fluid rounded"
+                                    src="/images/a2-4.jpg"
+                                    alt="Join the community!"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </section>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
