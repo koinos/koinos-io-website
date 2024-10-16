6a1406b47f8363f41c1c999eb8b2a37d0932ceab
Steve Gerbino <steve@gerbino.co>
2024-10-16 16:46:39 +0200

    Update X content text

--- pages/programs/lords-forsaken-quest.js
+++ pages/programs/lords-forsaken-quest.js
@@ -48,7 +48,7 @@ export default function LordsForsakenQuest() {
                       <li>Join <Link href="https://t.me/lordsforsaken" className="underline">Lord's Forsaken on Telegram</Link></li>
                       <li>Join <Link href="https://telegram.koinos.io/" className="underline">Koinos Telegram</Link></li>
                       <li><Link href="https://lordsforsaken.com/" className="underline">Play Lord's Forsaken</Link></li>
-                      <li>Post on <Link href="https://x.com/" className="underline">X</Link> with a screenshot of your victory -- be sure to include "@lordsforsak3n on @KoinosNetwork" in the text</li>
+                      <li>Post on <Link href="https://x.com/" className="underline">X</Link> with a screenshot of your victory -- be sure to include "Earn $FAITH by playing @lordsforsak3n on @KoinosNetwork " in the text</li>
                       <li>Join <Link href="https://taskon.xyz/Koinos" className="underline">Koinos on TaskOn</Link></li>
                     </ol>
 
