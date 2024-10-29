6041fdca70a0df66c7fd6006a6bdcb03351046d8
Steve Gerbino <steve@gerbino.co>
2024-10-29 16:43:15 +0100

    Delete unused elements

--- components/elements/Preloader.js
+++
@@ -1,9 +0,0 @@
-export default function Preloader() {
-    return (
-        <>
-            <div id="loading" className="loading--theme">
-                <div id="loading-center"><span className="loader" /></div>
-            </div>
-        </>
-    )
-}
