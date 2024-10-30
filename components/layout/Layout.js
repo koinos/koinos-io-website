c1f70256dd999a3fde83abacf5acb4b60ceae268
Steve Gerbino <steve@gerbino.co>
2024-10-30 19:11:56 +0100

    Clean up the footers and headers that are in use

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,10 +3,7 @@ import BackToTop from "../elements/BackToTop";
 import Breadcrumb from "./Breadcrumb";
 import PageHead from "./PageHead";
 import Footer1 from "./footer/Footer1";
-import Footer2 from "./footer/Footer2";
-import Footer3 from "./footer/Footer3";
 import Header1 from "./header/Header1";
-import Header2 from "./header/Header2";
 
 export default function Layout({
   headerStyle,
@@ -17,7 +14,7 @@ export default function Layout({
   headerCls,
 }) {
   const [scroll, setScroll] = useState(0);
-  // Moblile Menu
+  // Mobile Menu
   const [isMobileMenu, setMobileMenu] = useState(false);
   const handleMobileMenu = () => {
     setMobileMenu(!isMobileMenu);
@@ -25,21 +22,6 @@ export default function Layout({
       ? document.body.classList.add("wsactive")
       : document.body.classList.remove("wsactive");
   };
-
-  /*useEffect(() => {
-        const WOW = require('wowjs')
-        window.wow = new WOW.WOW({
-            live: false
-        })
-        window.wow.init()
-
-        document.addEventListener("scroll", () => {
-            const scrollCheck = window.scrollY > 100
-            if (scrollCheck !== scroll) {
-                setScroll(scrollCheck)
-            }
-        })
-    }, [])*/
 
   return (
     <>
@@ -61,14 +43,6 @@ export default function Layout({
             headerCls={headerCls}
           />
         ) : null}
-        {headerStyle == 2 ? (
-          <Header2
-            scroll={scroll}
-            isMobileMenu={isMobileMenu}
-            handleMobileMenu={handleMobileMenu}
-            headerCls={headerCls}
-          />
-        ) : null}
 
         <main className="main">
           {breadcrumbTitle && <Breadcrumb breadcrumbTitle={breadcrumbTitle} />}
@@ -78,8 +52,6 @@ export default function Layout({
 
         {!footerStyle && <Footer1 />}
         {footerStyle == 1 ? <Footer1 /> : null}
-        {footerStyle == 2 ? <Footer2 /> : null}
-        {footerStyle == 3 ? <Footer3 /> : null}
 
         <BackToTop />
       </div>
